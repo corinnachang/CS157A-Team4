95e279112c8a7823d190ee3f7cd4a0a943f6e98c
Corinna Chang <corinnachang11@gmail.com>
2022-07-03 16:35:47 -0700

    sql query for 'user' table

---
+++ MySQL codes/user table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `user` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
